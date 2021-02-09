5cf10454a462247b2e7df65324325328f37fcfcc
KatheAV <kaguilar31@misena.edu.co>
2021-02-08 21:19:45 -0500

    Category Seeder

--- larapp/database/seeders/DatabaseSeeder.php
+++ larapp/database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
         	
         	UserSeeder::class,
-        	//CategorySeeder::class,
+        	CategorySeeder::class,
         	//GameSeeder::class,
         ]);
     }
