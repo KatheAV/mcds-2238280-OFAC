53a5b77e08e579b400d78be3a9acaaa0771252d3
KatheAV <kaguilar31@misena.edu.co>
2021-02-10 21:06:54 -0500

    Reto Fabricas

--- larapp/database/factories/UserFactory.php
+++ larapp/database/factories/UserFactory.php
@@ -37,7 +37,7 @@ class UserFactory extends Factory
             'birthdate'         => $this->faker->dateTimeBetween($startDate = '-60 years', $endDate = '-21 years'),
             'gender'            => $gender,
             'address'           => $this->faker->streetAddress,
-            'photo'				=> $this->faker->image('public/imgs', 160, 160, 'people'),
+            'photo'				=> $this->faker->image($dir = 'public/imgs', 160, 160, 'persons'),
             'role'              => 'Editor',  
             'email_verified_at' => now(),
             'password'          => bcrypt('editor'), 
