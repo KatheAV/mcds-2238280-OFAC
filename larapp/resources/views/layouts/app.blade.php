c4ecdfd82925c0c769c783cd78fa75f0af7e826f
Katherine Aguilar <kaguilar31@misena.edu.co>
2021-04-27 20:39:24 -0500

    Export & Import Excel

--- larapp/resources/views/layouts/app.blade.php
+++ larapp/resources/views/layouts/app.blade.php
@@ -50,6 +50,13 @@
             $('.btn-upload').click(function() {
                 $('#photo').click();
             });
+             /* - - - - - - - - - - - - - - - - - - */
+            $('.btn-file').click(function() {
+                $('#file').click();
+            });
+            $('#file').change(function(event) {
+                $(this).parent().submit();
+            });
             /* - - - - - - - - - - - - - - - - - - */
             $('#photo').change(function(event) {
                 let reader = new FileReader();
